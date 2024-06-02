ab995c3a33ed9334f41777838db18e9384f82e1f
Emminence23 <137657093+Emminence23@users.noreply.github.com>
2024-06-02 23:48:59 +0530

    Delete Submission/231089_Tattwa Assignment 2 directory

--- Submission/231089_Tattwa Assignment 2/problem 4/hover.js
+++
@@ -1,8 +0,0 @@
-
-let element = document.getElementById('khazana');
-element.addEventListener("mouseenter", () => {
-    element.style.backgroundColor = "rgb(190, 5, 5)";
-});
-element.addEventListener("mouseleave", () => {
-    element.style.backgroundColor = "rgb(50, 18, 77)";
-});
