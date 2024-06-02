906d6a11bc484e730c279d6e7cc18f225b096cb7
Emminence23 <137657093+Emminence23@users.noreply.github.com>
2024-06-02 23:49:52 +0530

    submit 2

---
+++ Submission/Assignment_2/231089_Tattwa Assignment 2/problem 4/hover.js
@@ -0,0 +1,8 @@
+
+let element = document.getElementById('khazana');
+element.addEventListener("mouseenter", () => {
+    element.style.backgroundColor = "rgb(190, 5, 5)";
+});
+element.addEventListener("mouseleave", () => {
+    element.style.backgroundColor = "rgb(50, 18, 77)";
+});
